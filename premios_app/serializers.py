6eeff42b8c6b5e440b23aa7f4113b20aef8a5293
Collins Muriuki <murerwacollins@gmail.com>
2019-11-25 10:59:25 +0300

    Create serializer nested relationships

--- premios_app/serializers.py
+++ premios_app/serializers.py
@@ -1,17 +1,25 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Review
 from django.contrib.auth.models import User
 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ("id", "comment", "design_score", "usability_score",
+                  "content_score",)
+
+
 class ProjectSerializer(serializers.ModelSerializer):
-    reviews = serializers.StringRelatedField(many=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
     class Meta:
         model = Project
         fields = ("id", "author", "title", "description", "publish_date", 
-                  "project_pic", "live_site", "reviews") # to add author and reviews
+                  "project_pic", "live_site", "reviews",) # to add author and reviews
 
 
 class UserSerializer(serializers.ModelSerializer):
-    projects = serializers.StringRelatedField(many=True)
+    projects = ProjectSerializer(many=True, read_only=True)
     class Meta:
         model = User
         fields = ("id", "username", "email", "projects")
