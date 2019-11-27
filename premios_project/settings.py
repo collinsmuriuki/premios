9656073a290ff4377368bfa1228ac341f995a29c
Collins Muriuki <murerwacollins@gmail.com>
2019-11-27 09:31:48 +0300

    Switch to production

--- premios_project/settings.py
+++ premios_project/settings.py
@@ -26,9 +26,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://premi0s.herokuapp.com/']
 
 
 # Application definition
